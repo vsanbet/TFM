cb8dda27c54e92c27ef4d7c136b740f92af959f7
vsanbet <vsanbet@gmail.com>
2026-07-01 13:45:05 +0200

    Change x-axis distance to kilobases in LD plot
    Updated x-axis distance unit from base to kilobases.

--- general_pipeline/post_filtering/ld_plot.R
+++ general_pipeline/post_filtering/ld_plot.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 ld <- read.csv("C:/Users/Valeria/Downloads/LD_decay.csv")
 ld_sub <- ld[ld$Dist <= 10, ]
-ggplot(ld_sub, aes(x=Dist, y=Mean_r2)) +
+ggplot(ld_sub, aes(x=Dist/1000, y=Mean_r2)) +
   geom_line(color="steelblue", linewidth=1) +
   labs(x="Distance (kb)", y=expression(Mean~r^2), title="LD Decay") +
   theme_classic()
